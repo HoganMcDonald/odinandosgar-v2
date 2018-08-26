675daed08defcacd5f95fbf44016981928ad74f1
Hogan McDonald <hogan.developer@gmail.com>
2018-08-26 14:00:01 -0500

    mobile nav fades in and out

--- src/components/header/mobileNav/MobileNav.js
+++ src/components/header/mobileNav/MobileNav.js
@@ -7,16 +7,29 @@ import './MobileNav.scss';
 
 class MobileNav extends Component {
   className = this.props.className || 'mobile-nav';
+  state = {
+    isOpen: false
+  }
 
   componentDidMount() {
-    // this.nav.classList.add(`${this.className}--open`);
+    // delays setting open class until 1ms after mounting so long as isMounted is true
+    setTimeout(
+      () => {
+        this.setState({
+          isOpen: true
+        });
+      },
+      1
+    );
   }
 
   render() {
 
     return (
       <div 
-        className={classNames(this.className, {[`${this.className}--open`]: this.props.isMounted})} 
+        className={classNames(this.className, {
+          [`${this.className}--open`]: this.props.isMounted && this.state.isOpen
+        })} 
         ref={ref => this.nav = ref}>
 
       </div>
