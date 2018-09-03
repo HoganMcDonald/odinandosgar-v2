e9aa9fd2ca99c186da4757027db86dbf8179829f
Hogan McDonald <hogan.developer@gmail.com>
2018-09-02 21:14:10 -0500

    adds text to product tiles

--- src/components/productTile/ProductTile.js
+++ src/components/productTile/ProductTile.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ClampLines from 'react-clamp-lines';
 import _ from 'lodash';
 
 import { getFirstAvailableVariant } from 'helpers';
@@ -10,29 +12,37 @@ class ProductTile extends Component {
     const firstVariant = getFirstAvailableVariant(product.variants);
 
     return (
-      <article className="product-tile">
-        {
-          lazy && 
-          <div className="product-tile__content">
-            <div className="product-image product-image--lazy" />
-            <div className="product-title product-title--lazy" />
-            <div className="product-price product-price--lazy" />
-          </div>
-        }
+      // <Link to='/shop'>
+        <article className="product-tile">
+          {
+            lazy && 
+            <div className="product-tile__content">
+              <div className="product-image product-image--lazy" />
+              <div className="product-title product-title--lazy h4" />
+              <div className="product-price product-price--lazy h4" />
+            </div>
+          }
 
-        {
-          !lazy &&
-          <div className="product-tile__content">
-            <img src={_.get(product.images, '[0].src')} alt={_.get(product.images, '[0].alt')} className="product-image" />
-            <div className="product-title" >
-              <h4>{product.title}</h4>
+          {
+            !lazy &&
+            <div className="product-tile__content">
+              <img src={_.get(product.images, '[0].src')} alt={_.get(product.images, '[0].alt')} className="product-image" />
+              <div className="product-title" >
+                <h4>
+                  <ClampLines 
+                    text={product.title}
+                    lines='2'
+                    ellipsis='...'
+                    moreText={null} />
+                </h4>
+              </div>
+              <div className="product-price" >
+                <h4>{firstVariant.price}</h4>
+              </div>
             </div>
-            <div className="product-price" >
-              <h4>{firstVariant.price}</h4>
-            </div>
-          </div>
-        }
-      </article>
+          }
+        </article>
+      // </Link>
     );
   }
 }
